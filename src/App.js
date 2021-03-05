2a7c4162aa8f90f2638f131030ce0a4d0b888649
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-03-04 20:57:49 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Blog from './components/Blog.js'
+
+function App() {
+  return (
+    <div>
+      <Blog />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import {
-    Route,
-    Switch,
-    HashRouter
-} from "react-router-dom";
-
-import './assets/scss/style.scss';
-
-// pages for this product
-import Components from "./views/components/components.jsx";
-import CustomComponents from "./views/custom-components/custom-components.jsx";
-
-var hist = createBrowserHistory();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
-    <HashRouter history={hist}>
-        <Switch>
-            <Route path="/custom-components" component={CustomComponents} />
-            <Route path="/" component={Components} />
-        </Switch>
-    </HashRouter>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
